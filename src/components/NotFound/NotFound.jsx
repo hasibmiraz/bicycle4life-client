b803283ff3339553ae68782ad50f96c667c0330d
hasibmiraz <hasibmiraz09@gmail.com>
2022-06-06 13:52:27 +0600

    Checking github

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -10,6 +10,7 @@ const NotFound = () => {
         className="absolute h-full w-full object-cover"
         alt=""
       />
+
       <div className="inset-0 bg-black opacity-25 absolute"></div>
       <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
                {' '}
